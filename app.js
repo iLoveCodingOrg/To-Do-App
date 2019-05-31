f2599eb4ac9c23153250b64599a142646588645c
Noor Syed <noorsyed23@gmail.com>
2019-05-30 23:55:04 -0500

    step3

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const addButton = document.querySelector('#addbutton');
 const itemToAdd = document.querySelector('#itemToAdd');
+const ul = document.querySelector('.todo');
 
 addButton.addEventListener('click', function(){
-    console.log(itemToAdd.value);
-})
+    const newItem = createItem(itemToAdd.value);
+    ul.appendChild(newItem);
+});
+
+function createItem(val){
+    const item = document.createElement('li');
+    item.textContent = val;
+
+    return item;
+}
+
